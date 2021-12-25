b2c53fe7d5d6be38542c23b2f19bfcd6d883228d
Somesh Arora <somesh.arora@microsoft.com>
2021-12-25 10:33:43 -0800

    Initial Commit

---
+++ TradingJournal/TradingJournalApp.swift
@@ -0,0 +1,17 @@
+//
+//  TradingJournalApp.swift
+//  TradingJournal
+//
+//  Created by Somesh Arora on 12/25/21.
+//
+
+import SwiftUI
+
+@main
+struct TradingJournalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
